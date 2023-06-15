a7b4a23213d6f11081d7f4ba9a06c3170c3f43ba
wufeilong <wufeilong@yy.com>
2023-06-15 09:58:32 +0800

    update html

--- src/codevecdb/bert_vector.py
+++ src/codevecdb/bert_vector.py
@@ -1,10 +1,9 @@
-from langchain.embeddings import HuggingFaceEmbeddings, SentenceTransformerEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
 
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
 
 def semantics_vector(code):
-    print(type(code))
     print("this is my code: " + code)
     return embeddings.embed_query(code)
 
